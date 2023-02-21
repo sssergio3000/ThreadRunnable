84eba6bd6a7f907f508e25674d482bc77e4ca70c
Sergey Khaykov <ssserg3000@gmail.com>
2023-02-21 15:54:27 +0200

    update2

--- src/Task4/Main.java
+++ src/Task4/Main.java
@@ -3,17 +3,18 @@ package Task4;
 public class Main {
     public static void main(String[] args) {
 
-       BaseAlter ba1 = new ClassesAlter1(5);
-       BaseAlter ba2 = new ClassesAlter2(5);
+        BaseAlter ba1 = new ClassesAlter1(5);
+        BaseAlter ba2 = new ClassesAlter2(5);
 
-       Thread thr1 = new Thread(ba2);
-       Thread thr2 = new Thread(ba1);
+        Thread thr1 = new Thread(ba1);
+        Thread thr2 = new Thread(ba2);
 
+        ba1.setOtherThread(ba2);
+        ba2.setOtherThread(ba1);
 
-//
-            thr1.start();
-            thr2.start();
-//
+        thr1.start();
+        thr2.start();
+
 
     }
 }
